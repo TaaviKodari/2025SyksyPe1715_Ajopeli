cb4e407d1f8eecc97310596f71a0e3b697120ebf
DESKTOP-TEM8N3F\Mutti8888 <taavi.makela@gmail.com>
2025-11-07 18:16:17 +0200

    game manager aloitettu

--- 2025SyksyPe1715_Ajopeli/Assets/Omat Assetit/Scripts/Tuomari.cs
+++ 2025SyksyPe1715_Ajopeli/Assets/Omat Assetit/Scripts/Tuomari.cs
@@ -2,10 +2,36 @@ using UnityEngine;
 
 public class Tuomari : MonoBehaviour
 {
+    private bool winnerDeclared = false;
+
     private void OnTriggerEnter(Collider car)
     {
         CarIdentify id = car.GetComponent<CarIdentify>();
         string winnerName = id.displayName;
-        Debug.Log($"Winner: {winnerName}");
+
+
+        if (id.kind == CarKind.Player)
+        {
+            PelaajanKierrostarkistus validator = car.GetComponent<PelaajanKierrostarkistus>();
+            if (validator == null)
+            {
+                Debug.LogError("Missing PelaajanKierrostarkistus script");
+                return;
+            }
+
+            if (!validator.AllVisitedThisLap)
+            {
+                Debug.Log("Pelaaja ylitti maalin, mutta kaikki checkpointti eivät ole kunnossa -> Ei voittoa");
+                return;
+            }
+
+        }
+
+        if (!winnerDeclared)
+        {
+            Debug.Log($"WINNER:{winnerName}");
+            winnerDeclared = true;
+        }
+
     }
 }
