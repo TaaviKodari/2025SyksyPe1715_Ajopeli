da2a3e85adba0172e86386c6f1ee5959ef8b87e3
DESKTOP-TEM8N3F\Mutti8888 <taavi.makela@gmail.com>
2025-11-21 18:17:13 +0200

    countdown tehty valmiiksi

--- 2025SyksyPe1715_Ajopeli/Assets/Omat Assetit/Scripts/Countdown.cs
+++ 2025SyksyPe1715_Ajopeli/Assets/Omat Assetit/Scripts/Countdown.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections;
 using TMPro;
 public class Countdown : MonoBehaviour
 {
@@ -6,5 +7,26 @@ public class Countdown : MonoBehaviour
     public int countdownFrom = 3;
     public float stepSeconds = 1f;
 
+    IEnumerator Start()
+    {
+        //Debug.Log("Laskenta aloitettu");
+
+        for(int i = countdownFrom; i > 0; i--)
+        {
+            uiText.text = i.ToString();
+            yield return new WaitForSeconds(stepSeconds);
+        }
+
+        //Debug.Log("Laskenta lopetettu");
+
+        uiText.text = "GO!";
+
+        yield return new WaitForSeconds(0.5f);
+
+        uiText.text = "";
+        uiText.gameObject.SetActive(false);
+        
+        GameManager.Instance.Phase = RacePhase.Racing;
+    }
     
 }
